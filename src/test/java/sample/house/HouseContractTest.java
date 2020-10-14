9432dfc66ec59671a227248dc31a8427063c4ffc
Holly Cummins <cumminsh@uk.ibm.com>
2020-10-14 13:52:52 +0100

    Add provider contract test (with manual copying of contracts)

---
+++ src/test/java/sample/house/HouseContractTest.java
@@ -0,0 +1,30 @@
+package sample.house;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import au.com.dius.pact.provider.junit.IgnoreNoPactsToVerify;
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+
+@Provider("House")
+@PactFolder("pacts")
+@IgnoreNoPactsToVerify
+public class HouseContractTest {
+
+	@BeforeEach
+	void before(PactVerificationContext context) {
+		context.setTarget(new HttpTestTarget("localhost", 9080));
+	}
+
+	@TestTemplate
+	@ExtendWith(PactVerificationInvocationContextProvider.class)
+	void pactVerificationTestTemplate(PactVerificationContext context) {
+		context.verifyInteraction();
+	}
+
+}
